6ad5d6312e55dce073d94fce6b60cd5416f67509
Rus Esakov <eucle@ya.ru>
2023-08-11 13:52:44 +0300

    new solution added

---
+++ 448.py
@@ -0,0 +1,6 @@
+def findDisappearedNumbers(nums: list[int]) -> list[int]:
+    return list(set(range(1, len(nums) + 1)).difference(nums))
+
+
+print(findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1]))
+print(findDisappearedNumbers([1, 1]))
