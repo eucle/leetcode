c2de0dbd6bcb1aad573d7692799d710cfd4e73cd
Rus Esakov <eucle@ya.ru>
2023-08-12 08:26:36 +0300

    new solution added

---
+++ 392.py
@@ -0,0 +1,11 @@
+def isSubsequence(s: str, t: str) -> bool:
+    pointer1 = pointer2 = 0
+    while pointer1 < len(s) and pointer2 < len(t):
+        if s[pointer1] == t[pointer2]:
+            pointer1 += 1
+        pointer2 += 1
+    return pointer1 == len(s)
+
+
+print(isSubsequence("abc", "ahbgdc"))
+print(isSubsequence("axc", "ahbgdc"))
