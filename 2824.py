12ed73f5b115365b91c48ca37a12bd3cccd4961f
Rus Esakov <eucle@ya.ru>
2023-08-19 22:06:09 +0300

    new solution added

---
+++ 2824.py
@@ -0,0 +1,7 @@
+def countPairs(nums: list[int], target: int) -> int:
+    return sum([(nums[i]+nums[j] < 
+                 target) for i in range(len(nums)-1) for j in range(i+1, len(nums))])
+
+
+print(countPairs(nums=[-1, 1, 2, 3, 1], target=2))
+print(countPairs(nums=[-6, 2, 5, -2, -7, -1, 3], target=-2))
